1ae275d6ff7fb486b42ee2099882b04cf9cdd28f
Matheus Farias <farias.mts@outlook.com>
2021-10-18 11:05:45 -0300

    Creating front-end interfaces

--- atratividade/views.py
+++ atratividade/views.py
@@ -6,5 +6,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse('Teste')
+def home(request):
+    questions = [
+        'Ninguém aguenta mais o termo "cringe"',
+        'Eu sou humilde',
+        'Volta Domingão do Faustão',
+        'Volta Caldeirão',
+        'Tio Zuck espião',
+        'Flamengo é melhor time',
+        'Velozes e furiosos perdeu a graça',
+        'The Walking Dead precisa acabar',
+        'Sofrencia é bom',
+    ]
+    return render(request, 'home/index.html', {"questions":questions})
