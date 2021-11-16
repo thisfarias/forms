39c0b82c68bd56a9a2cad7df752bd530d4cedcc4
Matheus Farias <farias.mts@outlook.com>
2021-11-16 17:14:16 -0300

    New App 'Index'

--- forms/urls.py
+++ forms/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('atratividade/', include('atratividade.urls')),
-    path('ikigai/', include('ikigai.urls'))
+    path('ikigai/', include('ikigai.urls')),
+    path('', include('index.urls')),
 ]
+
+handler404 = 'inedx.views.handler_404'
---
+++ index/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    host = request.get_host()
+    ikigai = 'http://'+host+'/ikigai/forms'
+    atratividade = 'http://'+host+'/atratividade/forms'
+    return render(request, 'home.html', {'ikigai':ikigai, 'atratividade':atratividade})
+
+def handler_404(request):
+    response = render('404.html')
+    response.status_code = 404
+    return response
